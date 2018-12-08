8ebef5a77705a69d86558e1faba7ce35cb18ed44
zhoucs <zhoucs@192.168.100.2>
2018-12-08 19:34:12 +0800

    add test files and config test script’

---
+++ src/beautify.spec.ts
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+
+describe('Beautify function', () => {
+  // test character string 1
+  // const characStr = '';
+  it('should return ', () => {
+    expect(1 + 1).to.equal(2);
+  });
+});
